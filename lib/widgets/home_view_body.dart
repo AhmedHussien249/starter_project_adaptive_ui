354d9ed2beac06364061134a079368f9b7de57ed
ahmed hussien <ahmed.hussien950.ah@gmail.com>
2025-05-27 09:51:59 -0700

    refactor code and change layout widget to widget builder lazy building for good performance

--- lib/widgets/home_view_body.dart
+++ lib/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:starter_project_adaptive_ui/widgets/tablet_layout.dart';
 
+import 'adaptive_layout.dart';
 import 'desktop_layout.dart';
 import 'mobile_layout.dart';
 
@@ -9,18 +10,11 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return   Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: LayoutBuilder(builder: (context, constraints){
-        if(constraints.maxWidth < 600){
-          return const MobileLayout();
-        }else if (constraints.maxWidth < 900){
-          return const TabletLayout();
-        }else{
-          return const DesktopLayout();
-        }
-      }),
+      child: AdaptiveLayout(mobileLayout: (context) => const MobileLayout(), tabletLayout: (context) => const TabletLayout(), desktopLayout: (context) => const DesktopLayout(),),
     );
   }
 }
+
 
